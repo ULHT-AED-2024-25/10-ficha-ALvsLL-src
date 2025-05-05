1eebc4834b6ad71ae6c2143dbdcc2671c8bd39de
duarteneves <duartedbneves@gmail.com>
2025-05-05 10:42:24 +0100

    Set package.

---
+++ src/pt/ulusofona/aed/Pessoa.java
@@ -0,0 +1,22 @@
+package pt.ulusofona.aed;
+
+class Pessoa {
+
+    String nome;
+    String apelido;
+    int nrBI;
+    String paisMorada;
+
+
+    public Pessoa() {
+    }
+
+
+    public Pessoa(String nome, String apelido, int nrBi, String paisMorada) {
+        this.nome = nome;
+        this.apelido = apelido;
+        this.nrBI = nrBi;
+        this.paisMorada = paisMorada;
+    }
+
+}
